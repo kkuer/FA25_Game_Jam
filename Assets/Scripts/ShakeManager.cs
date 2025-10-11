ad7d1b844e18c12e0e810045a5dc6314b5335fb6
kris <kriskuerten@gmail.com>
2025-10-10 23:49:24 -0700

    camshake test

---
+++ Assets/Scripts/ShakeManager.cs
@@ -0,0 +1,54 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class ShakeManager : MonoBehaviour
+{
+    public static ShakeManager Instance { get; private set; }
+
+    private CinemachineCamera _cineCam;
+    private float shakeTimer;
+    private float initialShakeAmplitude;
+    private float totalShakeDuration;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(this.gameObject); }
+        else { Instance = this; }
+
+        _cineCam = GetComponent<CinemachineCamera>();
+    }
+
+    private void Update()
+    {
+        if (shakeTimer > 0f)
+        {
+            shakeTimer -= Time.deltaTime;
+
+            CinemachineBasicMultiChannelPerlin perlin = _cineCam.GetComponent<CinemachineBasicMultiChannelPerlin>();
+
+            float timeRatio = shakeTimer / totalShakeDuration;
+            perlin.AmplitudeGain = initialShakeAmplitude * timeRatio;
+
+            if (shakeTimer <= 0f)
+            {
+                perlin.AmplitudeGain = 0f;
+                perlin.FrequencyGain = 0f;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            shakeCam(3f, 0.1f, 0.2f);
+        }
+    }
+    public void shakeCam(float intensity, float frequency, float duration)
+    {
+        CinemachineBasicMultiChannelPerlin perlin = _cineCam.GetComponent<CinemachineBasicMultiChannelPerlin>();
+        perlin.AmplitudeGain = intensity;
+        perlin.FrequencyGain = frequency;
+
+        initialShakeAmplitude = intensity;
+        shakeTimer = duration;
+        totalShakeDuration = duration;
+    }
+}
