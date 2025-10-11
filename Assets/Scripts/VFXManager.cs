577a89e30346386b52ccb0d980520732161685b7
kris <kriskuerten@gmail.com>
2025-10-10 22:18:43 -0700

    particle methods
    call VFXManager.instance.playHit(), .playSlash()

--- Assets/Scripts/VFXManager.cs
+++ Assets/Scripts/VFXManager.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class VFXManager : MonoBehaviour
 {
+    [Header("Particle Prefabs")]
+    public GameObject slashParticles;
+    public GameObject hitParticles;
+
     public static VFXManager instance {  get; private set; }
     private void Awake()
     {
@@ -13,5 +17,28 @@ public class VFXManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void spawnVFX(GameObject particles, Vector3 pos, Quaternion rot)
+    {
+        GameObject newParticles = Instantiate(particles, pos, rot);
+        ParticleSystem ps = newParticles.GetComponent<ParticleSystem>();
+
+        ps.Play();
+    }
+
+    public void playVFX(ParticleSystem ps)
+    {
+        ps.Play();
+    }
+
+    public void playHit(Vector3 pos)
+    {
+        spawnVFX(hitParticles, pos, Quaternion.Euler(new Vector3(0, 0, 0)));
+    }
+
+    public void playSlash(ParticleSystem slashFX)
+    {
+        playVFX(slashFX);
     }
 }
