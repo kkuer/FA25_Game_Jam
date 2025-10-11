b4b1496e3f7761b9e636704ea93d24127d27e2a6
kris <kriskuerten@gmail.com>
2025-10-11 10:59:41 -0700

    gamemanager start

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro.Examples;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,7 +10,13 @@ public class GameManager : MonoBehaviour
     public bool gameActive;
     public bool gamePaused;
 
-    public int currentRoom;
+    public GameObject activeRoom;
+
+    public List<GameObject> roomPrefabs = new List<GameObject>();
+    public List<GameObject> enemyPrefabsW = new List<GameObject>();
+    public List<GameObject> enemyPrefabsB = new List<GameObject>();
+
+    public List<GameObject> activeEnemies = new List<GameObject>();
 
     private void Awake()
     {
@@ -23,9 +32,55 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        currentRoom = 0;
+        activeRoom = null;
         gameActive = true;
 
+        //spawn room
+        //spawn enemies
+        initializeNewRoom(4);
+
         //spawn in players
+    }
+
+    public void initializeNewRoom(int enemyAmount)
+    {
+        if (activeRoom != null)
+        {
+            Destroy(activeRoom.gameObject);
+            activeRoom = null;
+        }
+
+        GameObject chosenRoom = roomPrefabs[Random.Range(0, roomPrefabs.Count)];
+        GameObject newRoom = Instantiate(chosenRoom, Vector3.zero, Quaternion.identity, gameObject.transform);
+
+        activeRoom = newRoom;
+
+        Room r = newRoom.GetComponent<Room>();
+
+        //enemy spawner
+        if (r != null)
+        {
+            for (int i = 0; i < enemyAmount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    //spawn white piece
+                    GameObject newEnemy = Instantiate(enemyPrefabsW[Random.Range(0, enemyPrefabsW.Count)],
+                        r.enemySpawns[Random.Range(0, r.enemySpawns.Count)].position,
+                        Quaternion.identity);
+                    activeEnemies.Add(newEnemy);
+                }
+                else
+                {
+                    //spawn black piece
+                    GameObject newEnemy = Instantiate(enemyPrefabsB[Random.Range(0, enemyPrefabsB.Count)],
+                        r.enemySpawns[Random.Range(0, r.enemySpawns.Count)].position,
+                        Quaternion.identity);
+                    activeEnemies.Add(newEnemy);
+                }
+            }
+        }
+
+        //reference players
     }
 }
